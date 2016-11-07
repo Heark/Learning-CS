9a2da584a1c5fadd5812cedc908ac843da0870b2
Heark <adtune6509@student.waynecc.edu>
2016-11-07 14:58:29 -0500

    Create infiniteloop.cs

---
+++ infiniteloop.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfiniteLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1 };
+
+            for (int i = 0; i < 9999; i++)
+            {
+                if( i == 5000 ) {
+                    i = 0;
+                };
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
